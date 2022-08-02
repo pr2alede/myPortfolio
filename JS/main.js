197e6ece8792a0fa3509bafdd69eeecd682cc054
pr2alede <aled_evans_elli@yahoo.co.uk>
2022-08-02 14:05:30 +0100

    Adaptive nav bars added and anchors added

---
+++ JS/main.js
@@ -0,0 +1,33 @@
+let itemOne = document.getElementById("dropdown-content");
+let itemTwo = document.getElementById("menuLogo");
+let current = 0;
+
+function showItem() {
+    if (current ===0){
+        itemOne.style.display = "block";
+        current = 1;
+    }
+    else{
+        itemOne.style.display = "none";
+        current = 0;
+    }
+    
+
+ 
+  }
+
+
+  function checkMediaQuery() {
+    // If the inner width of the window is greater then 768px
+    if (window.innerWidth > 650) {
+      // Then log this message to the console
+      itemOne.style.display = "none";
+      let current = 0;
+    }
+  }
+  
+  // Add a listener for when the window resizes
+  window.addEventListener('resize', checkMediaQuery);
+
+  itemTwo.addEventListener('mousedown',showItem)
+  
